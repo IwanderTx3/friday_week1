50c28c3a8b1c5e670f19aa8599886424eefd6af2
V <DigitalCrafts@ms-MacBook-Pro.localdomain>
2018-05-04 14:52:00 -0500

    removed some blank lines

--- algorithmspractice_4.py
+++ algorithmspractice_4.py
@@ -5,8 +5,6 @@ while spaces > 80:
     print("Lets be resonable.  Keep the number under 80")
     spaces = int(input("How tall do you want the tree? "))
 
-
-
 while spaces > 0:
     print(" " * spaces + "*" * stars)
     spaces-=1
