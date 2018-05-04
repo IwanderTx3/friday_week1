400438fff550c304f2814b75a0052b6ad9ea19c2
V <DigitalCrafts@ms-MacBook-Pro.localdomain>
2018-05-04 12:20:01 -0500

    Added a max size of 80 to tree

--- algorithmspractice_4.py
+++ algorithmspractice_4.py
@@ -1,6 +1,12 @@
 spaces = int(input("How tall do you want the tree? "))
 stars = int(1)
 
+while spaces > 80:
+    print("Lets be resonable.  Keep the number under 80")
+    spaces = int(input("How tall do you want the tree? "))
+
+
+
 while spaces > 0:
     print(" " * spaces + "*" * stars)
     spaces-=1
