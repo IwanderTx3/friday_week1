fb33e033f0364ada299c2d58934e31512cedcd66
V <DigitalCrafts@ms-MacBook-Pro.localdomain>
2018-05-04 14:13:10 -0500

    replaced sort with a bubble sort

--- optional.py
+++ optional.py
@@ -3,24 +3,28 @@ sorting_bin = [1,4,5,346,63,2,1,6657,34,6,7,2346,67,7,3,23,46,8,564]
 
 print(sorting_bin)
 check = 0
-sort = input("Do you want to sort ascending or decending? (a or d) ")
 
-if sort.casefold() == 'a' :
-    while check+1 < len(sorting_bin):
-        if sorting_bin[check] <= sorting_bin[check+1]:
-            check += 1
-        else:
-            sorting_bin.append(sorting_bin[check])
-            sorting_bin.pop(check)
-            check=0
-elif sort.casefold() == 'd' :
-    while check+1 < len(sorting_bin):
-        if sorting_bin[check] >= sorting_bin[check+1]:
-            check += 1
-        else:
-            sorting_bin.append(sorting_bin[check])
-            sorting_bin.pop(check)
-            check=0
+
+def bubbleSort(sorting_bin):
+    sort = input("Do you want to sort ascending or decending? (a or d) ")
+    if sort == 'a' :
+        for passnum in range(len(sorting_bin)-1,0,-1):
+            for i in range(passnum):
+                if sorting_bin[i] > sorting_bin[i+1]:
+                    temp = sorting_bin[i]
+                    sorting_bin[i] = sorting_bin[i+1]
+                    sorting_bin[i+1] = temp 
+    elif sort == 'd' :
+        for passnum in range(len(sorting_bin)-1,0,-1):
+            for i in range(passnum):
+                if sorting_bin[i] < sorting_bin[i+1]:
+                    temp = sorting_bin[i]
+                    sorting_bin[i] = sorting_bin[i+1]
+                    sorting_bin[i+1] = temp
+    else:
+        print("Please select either a or d")
+
+bubbleSort(sorting_bin)
 print(sorting_bin)
 
 
